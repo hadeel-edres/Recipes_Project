edff9739f5db3328f59c935b81c6a3e1ae98223a
Hadeel Adres <hadeeledres@gmail.com>
2023-06-26 21:04:09 +0200

    add Validation

--- resources/views/admin/recipes/create.blade.php
+++ resources/views/admin/recipes/create.blade.php
@@ -19,8 +19,11 @@
                             <label for="name" class="block text-sm font-medium text-gray-700"> Name </label>
                             <div class="mt-1">
                                 <input type="text" id="name" name="name"
-                                    class="block w-full appearance-none bg-white border border-gray-400 rounded-md py-2 px-3 text-base leading-normal transition duration-150 ease-in-out sm:text-sm sm:leading-5" />
+                                    class="block w-full appearance-none bg-white border border-gray-400 rounded-md py-2 px-3 text-base leading-normal transition duration-150 ease-in-out sm:text-sm sm:leading-5 @error('name') border-red-400 @enderror" />
                             </div>
+                            @error('name')
+                            <div class="text-sm text-red-400">{{ $message }}</div>
+                            @enderror
 
                             <div class="sm:col-span-6 pt-5">
                             <label for="categories" class="block text-sm font-medium text-gray-700">Kategorien</label>
@@ -33,31 +36,46 @@
                                   </select></div>
                                   
    
-                                  <div class="sm:col-span-6 pt-5">
+                            <div class="sm:col-span-6 pt-5">
                             <label for="body" class="block text-sm font-medium text-gray-700">Beschreibung</label>
                             <div class="mt-1">
                                 <textarea id="body" rows="3" name="description"
-                                    class="shadow-sm focus:ring-indigo-500 appearance-none bg-white border py-2 px-3 text-base leading-normal transition duration-150 ease-in-out focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md"></textarea>
+                                    class="shadow-sm focus:ring-indigo-500 appearance-none bg-white border py-2 px-3 text-base leading-normal transition duration-150 ease-in-out focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md @error('description') border-red-400 @enderror "></textarea>
                             </div>
+                            @error('description')
+                                  <div class="text-sm text-red-400">{{ $message }}</div>
+                                  @enderror
 
-                                  <div class="sm:col-span-6 pt-5">
-    <label for="body" class="block text-sm font-medium text-gray-700">Zutaten</label>
-      <div class="mt-1">
-        <textarea id="body" rows="3" name="ingredients" class="shadow-sm focus:ring-indigo-500 appearance-none bg-white border border-gray-400 rounded-md py-2 px-3 text-base leading-normal transition duration-150 ease-in-out focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md"></textarea>
-      </div>
-    </div>
-    <div class="sm:col-span-6 pt-5">
+                        <div class="sm:col-span-6 pt-5">
+                         <label for="body" class="block text-sm font-medium text-gray-700">Zutaten</label>
+                              <div class="mt-1">
+                                <textarea id="body" rows="3" name="ingredients" class="shadow-sm focus:ring-indigo-500 appearance-none bg-white border border-gray-400 rounded-md py-2 px-3 text-base leading-normal transition duration-150 ease-in-out focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md @error('ingredients') border-red-400 @enderror"></textarea>
+                                 </div>
+                                 @error('ingredients')
+                                  <div class="text-sm text-red-400">{{ $message }}</div>
+                                  @enderror
+                                </div>
+
+                        <div class="sm:col-span-6 pt-5">
                             <label for="body" class="block text-sm font-medium text-gray-700">Zubereitung</label>
                             <div class="mt-1">
                                 <textarea id="body" rows="3" name="steps"
-                                    class="shadow-sm focus:ring-indigo-500 appearance-none bg-white border py-2 px-3 text-base leading-normal transition duration-150 ease-in-out focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md"></textarea>
+                                    class="shadow-sm focus:ring-indigo-500 appearance-none bg-white border py-2 px-3 text-base leading-normal transition duration-150 ease-in-out focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md @error('steps') border-red-400 @enderror"></textarea>
                             </div>
+                            @error('steps')
+                                  <div class="text-sm text-red-400">{{ $message }}</div>
+                                  @enderror
+                            </div>
+
                             <div class="sm:col-span-6">
                             <label for="image" class="block text-sm font-medium text-gray-700"> Image </label>
                             <div class="mt-1">
                                 <input type="file" id="image" name="image"
-                                    class="block w-full appearance-none bg-white border border-gray-400 rounded-md py-2 px-3 text-base leading-normal transition duration-150 ease-in-out sm:text-sm sm:leading-5" />
+                                    class="block w-full appearance-none bg-white border border-gray-400 rounded-md py-2 px-3 text-base leading-normal transition duration-150 ease-in-out sm:text-sm sm:leading-5 @error('image') border-red-400 @enderror" />
                             </div>
+                            @error('image')
+                                  <div class="text-sm text-red-400">{{ $message }}</div>
+                                  @enderror
     
                             </div>
                         <div class="mt-6 p-4">
--- resources/views/admin/recipes/edit.blade.php
+++ resources/views/admin/recipes/edit.blade.php
@@ -20,7 +20,11 @@
                             <label for="name" class="block text-sm font-medium text-gray-700"> Name </label>
                             <div class="mt-1">
                                 <input type="text" id="name" name="name" value= "{{ $recipe->name }}"
-                                    class="block w-full appearance-none bg-white border border-gray-400 rounded-md py-2 px-3 text-base leading-normal transition duration-150 ease-in-out sm:text-sm sm:leading-5" />
+                                    class="block w-full appearance-none bg-white border border-gray-400 rounded-md py-2 px-3 text-base leading-normal transition duration-150 ease-in-out sm:text-sm sm:leading-5 @error('name') border-red-400 @enderror" />
+                            </div>
+                            @error('name')
+                            <div class="text-sm text-red-400">{{ $message }}</div>
+                            @enderror
                             </div>
 
                             <div class="sm:col-span-6 pt-5">
@@ -37,37 +41,50 @@
         <label for="body" class="block text-sm font-medium text-gray-700">Beschreibung</label>
             <div class="mt-1">
                 <textarea id="body" rows="3" name="description" 
-                    class="shadow-sm focus:ring-indigo-500 appearance-none bg-white border py-2 px-3 text-base leading-normal transition duration-150 ease-in-out focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md">{{ $recipe->description }}</textarea>
+                    class="shadow-sm focus:ring-indigo-500 appearance-none bg-white border py-2 px-3 text-base leading-normal transition duration-150 ease-in-out focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md @error('description') border-red-400 @enderror">{{ $recipe->description }}</textarea>
+                     </div>
+                     @error('description')
+                                  <div class="text-sm text-red-400">{{ $message }}</div>
+                                  @enderror
                      </div>
    
     <div class="sm:col-span-6 pt-5">
     <label for="body" class="block text-sm font-medium text-gray-700">Zutaten</label>
       <div class="mt-1">
-        <textarea id="body" rows="3" name="ingredients"  class="shadow-sm focus:ring-indigo-500 appearance-none bg-white border border-gray-400 rounded-md py-2 px-3 text-base leading-normal transition duration-150 ease-in-out focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md">{{ $recipe->ingredients }}</textarea>
+        <textarea id="body" rows="3" name="ingredients"  class="shadow-sm focus:ring-indigo-500 appearance-none bg-white border border-gray-400 rounded-md py-2 px-3 text-base leading-normal transition duration-150 ease-in-out focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md @error('ingredients') border-red-400 @enderror">{{ $recipe->ingredients }}</textarea>
       </div>
+      @error('ingredients')
+        <div class="text-sm text-red-400">{{ $message }}</div>
+            @enderror
     </div>
 
     <div class="sm:col-span-6 pt-5">
     <label for="body" class="block text-sm font-medium text-gray-700">Zubereitung</label>
       <div class="mt-1">
-        <textarea id="body" rows="3" name="steps"  class="shadow-sm focus:ring-indigo-500 appearance-none bg-white border border-gray-400 rounded-md py-2 px-3 text-base leading-normal transition duration-150 ease-in-out focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md">{{ $recipe->steps }}</textarea>
+        <textarea id="body" rows="3" name="steps"  class="shadow-sm focus:ring-indigo-500 appearance-none bg-white border border-gray-400 rounded-md py-2 px-3 text-base leading-normal transition duration-150 ease-in-out focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md @error('steps') border-red-400 @enderror">{{ $recipe->steps }}</textarea>
       </div>
+      @error('steps')
+        <div class="text-sm text-red-400">{{ $message }}</div>
+        @enderror
     </div>
                            
-                            <div class="sm:col-span-6">
-                            <label for="image" class="block text-sm font-medium text-gray-700"> Image </label>
-                            <div>
-                       <img class="w-32 h-32" src="{{ Storage::url($recipe->image) }}">
-                        </div>
-                            <div class="mt-1">
-                                <input type="file" id="image" name="image"
-                                    class="block w-full appearance-none bg-white border border-gray-400 rounded-md py-2 px-3 text-base leading-normal transition duration-150 ease-in-out sm:text-sm sm:leading-5" />
-                            </div>
+        <div class="sm:col-span-6">
+                <label for="image" class="block text-sm font-medium text-gray-700"> Image </label>
+                <div>
+                    <img class="w-32 h-32" src="{{ Storage::url($recipe->image) }}">
+                </div>
+                <div class="mt-1">
+                    <input type="file" id="image" name="image"
+                         class="block w-full appearance-none bg-white border border-gray-400 rounded-md py-2 px-3 text-base leading-normal transition duration-150 ease-in-out sm:text-sm sm:leading-5 @error('image') border-red-400 @enderror" />
+                 </div>
+                 @error('image')
+                    <div class="text-sm text-red-400">{{ $message }}</div>
+                         @enderror
     
-                            </div>
+                </div>
                         <div class="mt-6 p-4">
                             <button type="submit"
-                                class="px-4 py-2 bg-indigo-500 hover:bg-indigo-700 rounded-lg text-white">Speichern</button>
+                                class="px-4 py-2 bg-indigo-500 hover:bg-indigo-700 rounded-lg text-white">Aktualisieren</button>
                         </div>
                     </form>
                 </div>
